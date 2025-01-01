10a9f073d2fa8482104de982394ca936d10d11f9
Dokjolly0 <alexviolattolibero.it@gmail.com>
2025-01-01 21:30:28 +0100

    Add browser to .env

--- src/open_driver.js
+++ src/open_driver.js
@@ -1,6 +1,8 @@
 const { Builder } = require("selenium-webdriver");
 const options = require("./option_driver");
+const dotenv = require("dotenv").config();
 
-driver = new Builder().forBrowser("chrome").setChromeOptions(options).build();
+const browser = process.env.BROWSER || "chrome";
+driver = new Builder().forBrowser(browser).setChromeOptions(options).build();
 
 module.exports = driver;
