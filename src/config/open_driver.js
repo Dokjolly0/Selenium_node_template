921ad71eec0826b0f7e8e743c617bc96949d7053
Dokjolly0 <alexviolattolibero.it@gmail.com>
2025-01-02 01:33:59 +0100

    Add option for each browser

--- src/config/open_driver.js
+++ src/config/open_driver.js
@@ -3,6 +3,15 @@ const options = require("./option_driver");
 const dotenv = require("dotenv").config();
 
 const browser = process.env.BROWSER || "chrome";
-driver = new Builder().forBrowser(browser).setChromeOptions(options).build();
+driver = new Builder()
+  .forBrowser(browser)
+  .setChromeOptions(browser === "chrome" ? options : undefined)
+  .setFirefoxOptions(browser === "firefox" ? options : undefined)
+  .setEdgeOptions(browser === "edge" ? options : undefined)
+  .setSafariOptions(browser === "safari" ? options : undefined)
+  .setOperaOptions(browser === "opera" ? options : undefined)
+  .setIEOptions(browser === "ie" ? options : undefined) // Internet Explorer
+  .setChromiumOptions(browser === "chromium" ? options : undefined)
+  .build();
 
 module.exports = driver;
